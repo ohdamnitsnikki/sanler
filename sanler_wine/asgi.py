46d5f1ac85eeed9f85b2e825815c2fb5a11e8d50
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-09-13 10:50:17 +0000

    deploymnt

---
+++ sanler_wine/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sanler_wine project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sanler_wine.settings')
+
+application = get_asgi_application()
