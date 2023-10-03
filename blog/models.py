2734e5725ffefae8bd9dcc13ef4f568658e2cafb
ohdamnitsnikki <nicole_stjernberg@hotmail.com>
2023-10-03 09:11:20 +0000

    create blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class BlogPost(models.Model):
+    headline = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='blog/images/', blank=True, null=True)
+    publication_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,31 @@ from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django.views.generic import ListView
+from .models import BlogPost
+from django.views.generic.edit import CreateView, DeleteView
+from django.urls import reverse_lazy
+from .forms import BlogPostForm
 
 
 def index(request):
     return render(
         request, 'index.html')
+
+
+class BlogPostListView(ListView):
+    model = BlogPost
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    ordering = ['-publication_date']  
+
+
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    form_class = BlogPostForm
+    template_name = 'blog/blogpost_form.html'
+    success_url = reverse_lazy('index')
+
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    success_url = reverse_lazy('index')
